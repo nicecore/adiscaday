0d11f4683712f4130befa8ff1d991666d729a598
Nicecore <23281721+nicecore@users.noreply.github.com>
2023-02-10 09:30:21 -0600

    about page updated

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='container'>
+      <p className='about-paragraph'><b>A Disc A Day</b> is a simple music review blog by me, Adam Cameron. Apart from being a place to practice my writing skills and publish my brief thoughts on what I hope will be many albums from a wide variety of genres, I've also built it to practice my web development skills and I will continue to refactor it and add features to it as time goes on. For those who care about that stuff, A Disc A Day's front end is powered by Next.js and Hygraph provides the back end. Like I said, simple! I hope this blog helps you discover new sounds.</p>
+      <p className='about-paragraph'>
+      Any questions, comments, etc. can be sent to <a href="mailto:adamdcameron@gmail.com">adamdcameron@gmail.com</a>. Thank you for reading!
+      </p>
+    </div>
   )
 }
 

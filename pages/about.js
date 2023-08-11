3d2303ea7b0d3e33ad7027478ac8c27a5849b2dc
Nicecore <23281721+nicecore@users.noreply.github.com>
2023-08-11 17:06:04 -0600

    abc

--- pages/about.js
+++ pages/about.js
@@ -3,12 +3,13 @@ import React from 'react'
 const About = () => {
   return (
     <div className='container'>
-      <p className='about-paragraph'><b>A Disc A Day</b> is a simple music review blog by me, Adam Cameron. Apart from being a place to practice my writing skills and publish my brief thoughts on what I hope will be many albums from a wide variety of genres, I've also built it to practice my web development skills and I will continue to refactor it and add features to it as time goes on. For those who care about that stuff, A Disc A Day's front end is powered by Next.js and Hygraph provides the back end. Like I said, simple! I hope this blog helps you discover new sounds.</p>
+      <p className='about-paragraph'><b>A Disc A Day</b> is a simple music review blog by me, Adam Cameron (link to other life stuff incoming once I have other life stuff to show off). Apart from being a place to practice my writing skills and publish my brief thoughts on what I hope will be many albums from a wide variety of genres, I've also built it to practice my web development skills and I will continue to refactor it and add features to it as time goes on. For those who care about that stuff, A Disc A Day's front end is powered by Next.js and Hygraph provides the back end. Like I said, simple! I hope this blog helps you discover new sounds.</p>
       <p>Some upcoming features include:</p>
       <ul>
         <li>• Improved UI</li>
         <li>• Comments on reviews</li>
         <li>• YouTube embeds in reviews so tracks can be listened to while you read my review</li>
+        <li>• Probably some kind of rating system, likely a tongue-in-cheek one as I don't really want to say any album is "bad"</li>
         <li>• Whatever else I can think of</li>
       </ul>
       <p className='about-paragraph'>
